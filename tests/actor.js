349f48da5848ae71d2f445cbdd5a50d81fdaf910
Nicolaas Matthijs <nicolaasmatthijs@googlemail.com>
2016-01-25 22:13:21 -0800

    Added Caliper conversion for courses, files and sessions

---
+++ tests/actor.js
@@ -0,0 +1,30 @@
+
+// User
+
+// xAPI
+
+// Email
+
+// Openid
+
+// Account
+
+// Caliper
+
+// TODO
+
+// Anonymous group
+
+// xAPI
+
+// Caliper
+
+// TODO
+
+// Identified group
+
+// xAPI
+
+// Caliper
+
+// TODO
